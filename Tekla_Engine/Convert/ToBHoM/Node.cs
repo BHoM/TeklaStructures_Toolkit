e8b3e8e653826ceb31ade431964cc2dd51010cc5
emidio piermarini <emidio.piermarini@burohappold.com>
2019-03-18 11:19:43 +0800

    Incorporate adapter template

---
+++ Tekla_Engine/Convert/ToBHoM/Node.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+
+namespace BH.Engine.Tekla
+{
+    public static partial class Convert
+    {
+        /***************************************************/
+        /**** Public Methods                            ****/
+        /***************************************************/
+
+        //Add methods for converting to BHoM from the specific software types. 
+        //Only do this if possible to do without any com-calls or similar to the adapter
+        //Example:
+        //public static Node ToBHoM(this TeklaNode node)
+        //{
+        //    //Insert code for convertion
+        //}
+
+        /***************************************************/
+    }
+}
