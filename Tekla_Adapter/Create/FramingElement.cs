d4e6230d4658af8e9daec077692a1155660c6afc
MartinHenriksen <martin.henriksen@burohappold.com>
2019-04-09 11:52:38 +0200

    adapter and read/write framingElement

---
+++ Tekla_Adapter/Create/FramingElement.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Structure.Elements;
+using BH.oM.Structure.Properties.Section;
+using BH.oM.Structure.Properties.Constraint;
+using BH.oM.Common.Materials;
+using BH.Engine.Tekla;
+
+using Tekla.Structures;
+using Tekla.Structures.Model;
+using Tekla.Structures.Filtering;
+
+
+namespace BH.Adapter.Tekla
+{
+    public partial class TeklaAdapter
+    {
+        private bool CreateCollection(IEnumerable<FramingElement> framingElements)
+        {
+            bool success = true;
+
+            foreach (FramingElement framing in framingElements)
+            {
+                //Tip: if the NextId method has been implemented you can get the id to be used for the creation out as (cast into applicable type used by the software):
+                int framingId = (int)framing.CustomData[AdapterId];
+                //If also the default implmentation for the DependencyTypes is used,
+                //one can from here get the id's of the subobjects by calling (cast into applicable type used by the software): 
+                //object startNodeId = bar.StartNode.CustomData[AdapterId];
+                //object endNodeId = bar.EndNode.CustomData[AdapterId];
+                //object SecPropId = bar.SectionProperty.CustomData[AdapterId];
+
+                Beam tsBeam = new Beam(framing.StructuralUsage.ToTekla());
+                tsBeam.StartPoint = framing.LocationCurve.Start.ToTekla();
+                tsBeam.EndPoint = framing.LocationCurve.End.ToTekla();
+                tsBeam.Identifier = new Identifier(framingId);
+                tsBeam.Name = framing.Name;
+                tsBeam.Position.Plane = Position.PlaneEnum.MIDDLE;
+                tsBeam.Position.Depth = Position.DepthEnum.MIDDLE;
+
+                if (m_ProfileLibrary.Contains(framing.Property.Name))
+                {
+                    tsBeam.Profile.ProfileString = framing.Property.Name;
+                }
+                else
+                {
+                    //add warning that profile does not exist and standard section has been used
+                    Engine.Reflection.Compute.RecordWarning("Profile " + framing.Property.Name + " was not found in library - replaced with: " + m_ProfileLibrary[0]);
+
+                    tsBeam.Profile.ProfileString = m_ProfileLibrary[0];// "HEA320";
+                }
+
+                if (!tsBeam.Insert())
+                    success = false;
+            }
+
+            return success;
+
+        }
+
+    }
+}
--- Tekla_Adapter/TeklaAdapter.cs
+++ Tekla_Adapter/TeklaAdapter.cs
@@ -20,6 +20,8 @@ namespace BH.Adapter.Tekla
         /**** Constructors                              ****/
         /***************************************************/
 
+        public List<string> connectionLibrary { get; set; } = new List<string>();
+
         //Add any applicable constructors here, such as linking to a specific file or anything else as well as linking to that file through the (if existing) com link via the API
         public TeklaAdapter(string filePath = "", bool active = false)
         {
@@ -36,11 +38,13 @@ namespace BH.Adapter.Tekla
 
                 m_TeklaModel = new Model();
                 m_CatalogHandler = new CatalogHandler();
+                m_ProfileLibrary = new List<string>();
+                m_ConnectionLibrary = new Dictionary<string, int>();
+
 
                 if (m_TeklaModel.GetConnectionStatus())
                 {
                     m_ObjectSelector = m_TeklaModel.GetModelObjectSelector();
-
                 }
                 else
                 {
@@ -50,9 +54,39 @@ namespace BH.Adapter.Tekla
                 if (m_CatalogHandler.GetConnectionStatus())
                 {
                     m_ComponentEnumerator = m_CatalogHandler.GetComponentItems();
-                    m_ProfileEnumerator = m_CatalogHandler.GetProfileItems();
                     m_MaterialEnumerator = m_CatalogHandler.GetMaterialItems();
-                    
+
+                    // build profile library
+                    ProfileItemEnumerator profileEnumerator = m_CatalogHandler.GetProfileItems();
+                    while (profileEnumerator.MoveNext())
+                    {
+                        LibraryProfileItem profileItem = profileEnumerator.Current as LibraryProfileItem;
+                        if (profileItem != null && !string.IsNullOrEmpty(profileItem.ProfileName))
+                            m_ProfileLibrary.Add(profileItem.ProfileName);
+                    }
+
+                    //build connection library
+                    while (m_ComponentEnumerator.MoveNext())
+                    {
+                        ComponentItem item = m_ComponentEnumerator.Current as ComponentItem;
+
+                        if (item.Type == ComponentItem.ComponentTypeEnum.CONNECTION)
+                        {
+                            // NOTE 
+                            // item.Name is mostly empty. when it is not empty is a duplicate of item.UIName and in this case the item.Nunber is -200000
+                            // examples in documentation show connections created from item.Number but it could be that when this is -200000 it can be created from item.Name
+                            if (!m_ConnectionLibrary.ContainsKey(item.UIName))
+                            {
+                                m_ConnectionLibrary.Add(item.UIName, item.Number);
+                            }
+                            else
+                            {
+                                // add some warning ! ! ! ! ! ! 
+                            }
+                        }
+                    }
+
+                    connectionLibrary.AddRange(m_ConnectionLibrary.Keys.ToList());
                 }
                 else
                 {
@@ -78,10 +112,11 @@ namespace BH.Adapter.Tekla
         private CatalogHandler m_CatalogHandler;
         private ModelObjectSelector m_ObjectSelector;
         private ComponentItemEnumerator m_ComponentEnumerator;
-        private ProfileItemEnumerator m_ProfileEnumerator;
+        private List<string> m_ProfileLibrary;//---There is curently no point in storing a dictionary with the profile properties; only name is used to create framing elements
+        private Dictionary<string, int> m_ConnectionLibrary;
         private MaterialItemEnumerator m_MaterialEnumerator;
 
-        
+
         /***************************************************/
 
 
