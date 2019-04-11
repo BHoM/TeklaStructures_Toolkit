b9a5b9746cb10e24e60521fdaeaacd6c25501a23
MartinHenriksen <martin.henriksen@burohappold.com>
2019-04-11 10:27:51 +0200

    initial prototype of Connection sub-parts

---
+++ Tekla_oM/Bolt.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+using BH.oM.Geometry;
+using BH.oM.Structure.Properties.Section;
+using BH.oM.Structure.Properties.Constraint;
+using BH.oM.Structure.Properties;
+
+namespace BH.oM.Structure.Elements
+{
+    public class Bolt : BHoMObject
+    {
+        // type enum = site/Workshop/ ... 
+
+        public double Size { get; set; }
+
+        public string Standard { get; set; }
+
+
+
+    }
+
+}
---
+++ Tekla_oM/Weld.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using BH.oM.Base;
+using BH.oM.Geometry;
+using BH.oM.Structure.Properties.Section;
+using BH.oM.Structure.Properties.Constraint;
+using BH.oM.Structure.Properties;
+
+namespace BH.oM.Structure.Elements
+{
+    public class Weld : BHoMObject
+    {
+        // connecting parts (plate/framingElement/...)
+
+        // WeldContour Enum (none/flush/convex/concave/...)
+
+        // Pitch double
+
+        // Position (Line ??)
+
+        // WeldProcess Enum (shielded metal/submerged arc/gas metal arc/flux cored arc/electroslag/electrogas)
+
+        // WeldType (shop weld/site weld)
+
+
+    }
+
+}
