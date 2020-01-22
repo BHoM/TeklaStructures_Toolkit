dfd346c0b992fe239e29bf859ee636bd2db5823f
MartinHenriksen <martin.henriksen@burohappold.com>
2020-01-22 17:26:23 +0100

    refactor ChangeWorkPlane to use IExecuteCommand

--- Tekla_Adapter_Commands/Commands.cs
+++ Tekla_Adapter_Commands/Commands.cs
@@ -8,18 +8,16 @@ using BH.oM.Adapter;
 using BH.oM.Adapter.Commands;
 using BH.oM.Geometry;
 using BH.oM.Reflection;
-using Tekla.Structures;
-using Tekla.Structures.Model;
-using tsGeo = Tekla.Structures.Geometry3d;
+
 
 namespace BH.oM.Adapter.Commands.Tekla
 {
     public class ChangeWorkPlane : IExecuteCommand, IObject
     {
         public BH.oM.Geometry.Plane Plane { get; set; } = null;
-
-        public BH.oM.Geometry.Vector XVextor { get; set; } = null;
-        public BH.oM.Geometry.Vector YVextor { get; set; } = null;
-
+        public BH.oM.Geometry.Vector XVector { get; set; } = null;
+        public BH.oM.Geometry.Vector YVector { get; set; } = null;
+        public BH.oM.Geometry.Point Origin { get; set; } = null;
 
     }
+}
