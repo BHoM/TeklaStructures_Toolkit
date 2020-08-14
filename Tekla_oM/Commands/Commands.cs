d966794c620c58b3360f8d147d1f781985ed6124
Isak Naslund <Isak.Naslund@burohappold.com>
2020-08-14 16:57:30 +0200

    Remove commandss project, migrated to oM

---
+++ Tekla_oM/Commands/Commands.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BH.oM.Base;
+using BH.oM.Adapter;
+using BH.oM.Adapter.Commands;
+using BH.oM.Geometry;
+using BH.oM.Reflection;
+
+
+namespace BH.oM.Adapters.Tekla.Commands
+{
+    public class ChangeWorkPlane : IExecuteCommand, IObject
+    {
+        public BH.oM.Geometry.Plane Plane { get; set; } = null;
+        public BH.oM.Geometry.Vector XVector { get; set; } = null;
+        public BH.oM.Geometry.Vector YVector { get; set; } = null;
+        public BH.oM.Geometry.Point Origin { get; set; } = null;
+
+    }
+}
